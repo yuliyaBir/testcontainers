9835935389e2edc990b3c84534426f85c61d5c1b
yuliyaBir <zhivo4ka@gmail.com>
2024-01-16 09:59:20 +0100

    Initial commit

---
+++ src/main/java/be/vdab/testcontainers/PersoonRepository.java
@@ -0,0 +1,2 @@
+package be.vdab.testcontainers;public interface PersoonRepository {
+}
---
+++ src/test/java/be/vdab/testcontainers/TestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
